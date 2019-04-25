4ea07a8f7ab3ff29801ebe66f181bdd3a8330b79
yukitohiroki <qfpng09604@yahoo.co.jp>
2019-04-25 21:13:46 +0900

    Edit-deploy3
    デプロイのエラー解消のため

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "mercari_weekend_46nagoya"
-set :repo_url, "git@example.com:yukitohiroki/mercari_weekend_46nagoya.git"
+set :repo_url, "git@github.com:yukitohiroki/mercari_weekend_46nagoya.git"
 
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
