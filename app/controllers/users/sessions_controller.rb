7db8d0ab2bd5f557aa455bad05cf8b600cf3a6a9
yukitohiroki <qfpng09604@yahoo.co.jp>
2019-05-02 17:18:23 +0900

    Edit-recaptcha
    recaptchaのviewの修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,16 @@
-class SessionsController < Devise::SessionsController
-  def create
-    if verify_recaptcha
-      super
-    else
-      self.resource = resource_class.new
-      respond_with_navigational(resource) { render :new }
-    end
-  end
+class Users::SessionsController < Devise::SessionsController
+  # before_action :check_recaptcha, only: %i(create)
+
+  # def create
+  #   super
+  # end
+
+  # protected
+  # def check_recaptcha
+  #   unless verify_recaptcha
+  #     self.resource = resource_class.new sign_in_params
+  #     resource.validate
+  #     respond_with_navigational(resource) { render :new }
+  #   end
+  # end
 end
