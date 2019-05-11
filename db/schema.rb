e5405bbbbb78ee0a3d2f397f4783f453d8683536
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-05-11 16:34:25 +0900

    create-first_categories_table
    categoriesテーブルの作成
    アソシエーションの記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_070624) do
+ActiveRecord::Schema.define(version: 2019_05_11_071124) do
 
   create_table "conditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "condition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "first_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_category", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
