6e2ca18f8589fa45eb362d784c65ea5cae4ed67a
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-05-11 19:37:26 +0900

    create-rates_table
    ratesテーブルの作成
    アソシエーションの記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_102433) do
+ActiveRecord::Schema.define(version: 2019_05_11_103308) do
 
   create_table "brand_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "brand_id"
@@ -92,6 +92,12 @@ ActiveRecord::Schema.define(version: 2019_05_11_102433) do
   create_table "rate_counts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "message", null: false
     t.boolean "seller_or_buyer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
