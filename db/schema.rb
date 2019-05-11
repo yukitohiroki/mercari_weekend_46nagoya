96c187175c316059c5ccc0b153cd816f436e8fd7
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-05-11 16:10:37 +0900

    create-conditions_table
    conditionsテーブルの作成
    アソシエーションの記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_052726) do
+ActiveRecord::Schema.define(version: 2019_05_11_070624) do
+
+  create_table "conditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "condition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user", null: false
