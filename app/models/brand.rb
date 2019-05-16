26b63f0e1e1946f2af7c390f388ff76b324c0fac
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-05-16 12:07:56 +0900

    correction
    微修正

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ApplicationRecord
   has_many :items
-  has_many :first_categories,through::brand_categories
+  has_many :first_categories,through: :brand_categories
   has_many :brand_categories
 end
