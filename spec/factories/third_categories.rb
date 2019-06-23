1ec08ab673d5ebbc7f7e95ef75fb84b96e5d654d
yukitohiroki <qfpng09604@yahoo.co.jp>
2019-06-23 16:18:48 +0900

    create-edit-test
    商品の編集と削除機能のテストを実装
    また、商品カテゴリーのテストが漏れていたため、そちらも実装済み

--- spec/factories/third_categories.rb
+++ spec/factories/third_categories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :third_category do
-    
+    third_category    { "c" }
+    association :second_category, factory: :second_category
+    initialize_with   { ThirdCategory.find_or_create_by(id: 1,third_category: 'c')}
   end
 end
