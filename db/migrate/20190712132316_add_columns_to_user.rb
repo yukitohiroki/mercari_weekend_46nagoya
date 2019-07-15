5724325b64ce696fdd37bd47557224e560d558bf
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-07-15 15:50:27 +0900

    correction
    修正
    null制約の追加→取り外し

--- db/migrate/20190712132316_add_columns_to_user.rb
+++ db/migrate/20190712132316_add_columns_to_user.rb
@@ -1,12 +1,12 @@
 class AddColumnsToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :expert_or_ordinary, :boolean
-    add_column :users, :company, :text ,null: false
-    add_column :users, :job, :text ,null: false
-    add_column :users, :position, :text ,null: false
-    add_column :users, :work_year, :integer ,null: false
-    add_column :users, :first_category_id, :integer ,null: false
-    add_column :users, :second_category_id, :integer ,null: false
-    add_column :users, :third_category_id, :integer ,null: false
+    add_column :users, :company, :text 
+    add_column :users, :job, :text 
+    add_column :users, :position, :text 
+    add_column :users, :work_year, :integer
+    add_column :users, :first_category_id, :integer
+    add_column :users, :second_category_id, :integer
+    add_column :users, :third_category_id, :integer
   end
 end
